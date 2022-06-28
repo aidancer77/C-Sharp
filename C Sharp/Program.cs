6ea6b888e8a9f74a38362f994c1c48885900a4bb
aidancer55 <bluebonnets77@gmail.com>
2022-06-28 16:59:00 +0300

    Добавьте файлы проекта.

---
+++ C Sharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int R = Convert.ToInt32(Console.ReadLine());
+            double S1;
+            S1 = Math.PI * Math.Pow(R, 2);
+            S1 = Math.Round(S1, 1);
+            Console.WriteLine("Радиус окружности = {0}", R);
+            Console.WriteLine("Площадь окружности = {0}", S1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double p, S2;
+
+            if (a * a + b * b == c * c)
+            {
+                S2 = (a * b) / 2;
+                Console.WriteLine("Треугольник является прямоугольным, площадь S = {0}", S2);
+            }
+            else
+            {
+                p = (a + b + c) / 2;
+                p = Math.Round(p, 1);
+                S2 = Convert.ToDouble(Math.Sqrt(p * ((p - a) * (p - b) * (p - c))));
+                S2 = Math.Round(S2, 1);
+                Console.WriteLine("Площадь треугольника S = {0}", S2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
